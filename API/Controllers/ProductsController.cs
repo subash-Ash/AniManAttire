8ff04615a6788bde7cce9ab41d4cfe1b1979db8f
subash-Ash <73168338+subash-Ash@users.noreply.github.com>
2023-09-24 15:44:57 +0530

    Implemented Generics

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -10,35 +10,43 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly IProductRepository _repository;
+        private readonly IGenericRepository<Product> _productsRepo;
 
-        public ProductsController(IProductRepository repository)
+        private readonly IGenericRepository<ProductBrand> _brandRepo;
+
+        private readonly IGenericRepository<ProductType> _productTypeRepo;
+
+        public ProductsController(IGenericRepository<Product> productsRepo, IGenericRepository<ProductBrand> brandRepo
+            ,IGenericRepository<ProductType> productTypeRepo)
         {
-            _repository = repository;
+            _productsRepo= productsRepo;
+            _brandRepo= brandRepo;
+            _productTypeRepo= productTypeRepo;
         }
+
         [HttpGet("Products")]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _repository.GetProductsAsync();
+            var products = await _productsRepo.GetAllAsync();
             return Ok(products);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProducts(int id)
         {
-            return await _repository.GetProductByIdAsync(id);
+            return await _productsRepo.GetByIdAsync(id);
         }
 
         [HttpGet("Brands")]
         public async Task<ActionResult<List<ProductBrand>>> GetProductBrandsAsync()
         {
-            return Ok(await _repository.GetProductBrandsAsync());
+            return Ok(await _brandRepo.GetAllAsync());
         }
 
         [HttpGet("Types")]
         public async Task<ActionResult<List<ProductType>>> GetProductTypesAsync()
         {
-            return Ok(await _repository.GetProductTypesAsync());
+            return Ok(await _productTypeRepo.GetAllAsync());
         }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -17,12 +17,13 @@ internal class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
-        builder.Services.AddDbContext<StoreContext>(x => x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))); //default
+       
         builder.Services.AddScoped<IProductRepository, ProductRepository>(); /* So, when you register IProductRepository as scoped, it means that a new instance of ProductRepository will be created for each incoming HTTP request, and it will be available to all components (controllers, middleware, etc.) that need it within the scope of that request. This helps ensure that different requests do not interfere with each other and can have their own isolated instances of the repository.
-
+        
         This is a common pattern in ASP.NET Core for setting up dependency injection for your application's services, making your code more maintainable, testable, and loosely coupled.*/
         
-
+        builder.Services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+        builder.Services.AddDbContext<StoreContext>(x => x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))); //default
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
